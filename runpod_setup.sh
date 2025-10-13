292cc8ef9cbbb2c8d4168612e404360f201640f8
milan-kodali <milankodali@gmail.com>
2025-10-13 22:36:05 +0000

    continue runpod setup

--- runpod_setup.sh
+++ runpod_setup.sh
@@ -1,5 +1,28 @@
-# Set global Git identity
+# setup aws cli
+echo "setting up aws cli"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip aws
+aws --version
+echo -e "aws cli setup complete\n-----"
+
+# Exit immediately if any future command fails
+set -e 
+
+# Set Git identity
 echo "setting up git config"
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 echo -e "git config complete\n-----"
+
+# install python reqs
+echo "installing requirements\n-----"
+pip install -r requirements.txt
+echo -e "-----\nrequirements installed\n-----"
+
+# set default shell working dir to mk-gpt repo
+echo "setting default shell working dir"
+echo 'cd /workspace/mk-gpt' >> ~/.bashrc
+source ~/.bashrc
+echo -e "default shell working dir set\n-----"
